7037871d3e1f1b78a9967c88eaad0b9da282433f
wmlynar <wojciech.mlynarczyk+github@gmail.com>
2017-08-27 17:42:12 +0200

    model fix

--- src/main/java/com/github/wmlynar/rosjava_gyro_calibration/ekf/LinearMovementModel.java
+++ src/main/java/com/github/wmlynar/rosjava_gyro_calibration/ekf/LinearMovementModel.java
@@ -33,8 +33,8 @@ public class LinearMovementModel {
 	public Matrix getJacobian(Matrix state_estimate, double dt) {
 		jacobian.data[0][0] = 1;
 		jacobian.data[0][1] = dt;
-		jacobian.data[1][0] = 1;
-		jacobian.data[1][1] = 0;
+		jacobian.data[1][0] = 0;
+		jacobian.data[1][1] = 1;
 		return jacobian;
 	}
 
